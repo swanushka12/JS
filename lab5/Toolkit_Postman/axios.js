6c55de039ec7c55ac3883f07a186896176123814
swanushka12 <115592590+swanushka12@users.noreply.github.com>
2025-05-03 02:02:06 +0300

    Update axios.js

--- lab5/Toolkit_Postman/axios.js
+++ lab5/Toolkit_Postman/axios.js
@@ -1,10 +1,10 @@
 // для nodejs
 const axios = require('axios');
 
-axios.get('https://vk.com')
+axios.get('https://vk.com');
 .then(response => 
 {
-    console.log('Успех:', response.status)
+    console.log('Успех:', response.status);
 })
 .catch(error => 
 {
@@ -14,7 +14,7 @@ axios.get('https://vk.com')
 // Успех: 200
 // запрос с Node.js будет выполнен без ограничений CORS
 
-axios.get('https://json.geoiplookup.io/')
+axios.get('https://json.geoiplookup.io/');
 .then(response => 
 {
     console.log('Успех:', response.status);
