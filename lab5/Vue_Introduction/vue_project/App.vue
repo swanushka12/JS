bb539b4f024c18cb6fb5a074251c1cdf9b540b4c
swanushka12 <115592590+swanushka12@users.noreply.github.com>
2025-05-03 15:23:36 +0300

    Add files via upload

---
+++ lab5/Vue_Introduction/vue_project/App.vue
@@ -0,0 +1,33 @@
+// vue_project\src\App.vue
+
+<template>
+  <div id="app">
+    <h1>100 Clickers</h1>
+      <div v-for="n in 100" :key="n">
+        <Clicker :amount="1" @button-clicked="click(n)">
+          Click
+        </Clicker>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue';
+import Clicker from './components/clicker.vue';
+
+const emit = defineEmits();
+
+const click = (n) => 
+{
+  alert(`Clicker ${n} clicked!`);
+};
+</script>
+    
+<style>
+button 
+{    
+  padding: 10px 20px;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
---
+++ lab5/Vue_Introduction/vue_project/clicker.vue
@@ -0,0 +1,43 @@
+// vue_project\src\components\clicker.vue
+
+<template>
+  <div>    
+    <button @click="click">Click</button>
+    <p>Clicked {{ count }} times</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+// props
+const props = defineProps({
+    amount: 
+    {
+        type: Number,
+        default: 1
+    }
+});
+
+// количество кликов
+const count = ref(0);
+
+// событие
+const event = defineEmits(['button-clicked']);
+
+// обработка клика
+const click = () => 
+{
+    count.value += props.amount; 
+    event('button-clicked'); 
+};
+</script>
+
+<style scoped>
+button 
+{
+    padding: 10px 20px;
+    font-size: 20px;
+    cursor: pointer;
+}
+</style>
